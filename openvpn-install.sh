b567c5e0639a324ead602067a0f4194f99813cc5
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2024-07-18 14:18:31 -0300

    fix: use ifconfig.me to get public ip

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -244,7 +244,7 @@ function installQuestions() {
 		echo "It seems this server is behind NAT. What is its public IPv4 address or hostname?"
 		echo "We need it for the clients to connect to the server."
 
-		PUBLICIP=$(curl -s https://api.ipify.org)
+		PUBLICIP=$(curl -s https://ifconfig.me)
 		until [[ $ENDPOINT != "" ]]; do
 			read -rp "Public IPv4 address or hostname: " -e -i "$PUBLICIP" ENDPOINT
 		done
